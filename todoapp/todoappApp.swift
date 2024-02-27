7319825d32bb91d632214948df631450b87b3cc1
aergul <alpyergul@gmail.com>
2024-02-27 23:21:30 +0300

    Initial Commit

---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Alpay Ergül on 27.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
