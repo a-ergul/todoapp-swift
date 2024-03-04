1ac610af39d613e43f8979eb07bc6882163f845e
aergul <alpyergul@gmail.com>
2024-03-04 23:01:20 +0300

    added check button

--- todoapp/ViewModels/ToDoListItemViewViewModel.swift
+++ todoapp/ViewModels/ToDoListItemViewViewModel.swift
@@ -4,9 +4,9 @@
 //
 //  Created by Alpay Ergül on 27.02.2024.
 //
-
+import FirebaseFirestore
 import Foundation
-
+import FirebaseAuth
 
 class ToDoListItemViewViewModel: ObservableObject{
     init(){
@@ -14,6 +14,18 @@ class ToDoListItemViewViewModel: ObservableObject{
     }
     
     func toggleIsDone(item: ToDoListItem){
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(item.id)
+            .setData(itemCopy.asDictionary())
         
     }
 }
--- todoapp/Views/ToDoListItemView.swift
+++ todoapp/Views/ToDoListItemView.swift
@@ -25,6 +25,7 @@ struct ToDoListItemView: View {
                 viewModel.toggleIsDone(item: item)
             } label: {
                 Image(systemName: item.isDone ? "checkmark.circle.fill" : "circle")
+                    .foregroundStyle(Color.blue)
             }
         }
     }
